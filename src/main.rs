0a0b47e789cbf2bcb1a40f93db3f5a693bc19404
akicho8 <akicho8@gmail.com>
2022-02-09 18:26:16 +0900

    [feat] cat command

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,55 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
-// use std::collections::HashMap;
+use std::collections::HashMap;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
-    let f: fn() = my_func1;
-    f();
+    // let f: fn() = command_cat;
+    // f();
 
-    println!("{:?}", args);
-    println!("command = {}", command);
+    // println!("{:?}", args);
+    // println!("command = {}", command);
 
     // println!("command = {}", foo());
 
-    if command == "cat" {
-        let filename = &args[2];
-        let mut f = File::open(filename).expect("ファイルが見つかりません");
-        let mut contents = String::new();
-        f.read_to_string(&mut contents).expect("失敗しました");
-        println!("{}", contents);
-    }
+    // if command == "cat" {
+    //     let filename = &args[2];
+    //     let mut f = File::open(filename).expect("ファイルが見つかりません");
+    //     let mut contents = String::new();
+    //     f.read_to_string(&mut contents).expect("失敗しました");
+    //     println!("{}", contents);
+    // }
 
-    let my_closure1 = || println!("my_closure1");
-    call_me(my_closure1);
-    call_me(my_func1);
+    // let my_closure1 = || println!("my_closure1");
+    // call_me(my_closure1);
+    // call_me(command_cat);
 
     // let mut scores = HashMap::new();
-    // scores.insert(String::from("Key1"), 10);
+    // scores.insert(String::from("Cat"), 10);
     // scores.insert(String::from("Yellow"), 50);
-    // let key = String::from("Key1");
+    // let key = String::from("Cat");
     // let a = scores.get(&key);
     // println!("{:?}", a);
     //
-    // // let mut map2: HashMap<&str, fn()> = HashMap::new();
-    // let mut map2 = HashMap::new();
-    // map2.insert(String::from("key1"), my_func1);
-    // let _key1 = String::from("key1");
 
-    // let f3: <fn()> = map2.get(key1);
-    // // f3();
+    // // let mut function_map: HashMap<&str, fn()> = HashMap::new();
+    let mut function_map = HashMap::new();
+    function_map.insert(String::from("cat"), command_cat);
+    let key = String::from(command);
+    let found_func = function_map.get(&key).unwrap();
+    found_func();
 
-    // let f3 = map2.get(&key1);
-    // f3;
+    // let found_func = function_map.get(&cat);
+    // found_func;
 
-    // println!("{:?}", map2.get(&key1));
+    // println!("{:?}", function_map.get(&cat));
 
     // // scores.insert(String::from("Yellow"), 50);
-    // let key2 = String::from("Key1");
-    // let a1 = map2.get(&key2);
+    // let key2 = String::from("Cat");
+    // let a1 = function_map.get(&key2);
     // println!("{:?}", a1);
 
     // let solar_distance = HashMap::from([
@@ -65,10 +65,15 @@ fn main() {
 //     1
 // }
 
-fn call_me<F1: Fn()>(f2: F1) {
-    f2();
-}
+// fn call_me<F1: Fn()>(f2: F1) {
+//     f2();
+// }
 
-fn my_func1() {
-    println!("my_func1");
+fn command_cat() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[2];
+    let mut f = File::open(filename).expect("ファイルが見つかりません");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("失敗しました");
+    print!("{}", contents);
 }
